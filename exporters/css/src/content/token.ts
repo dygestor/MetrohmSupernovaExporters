9652c91f75e613479f5bb4f39a8bd7af2df1c223
Hledík, Ján <jan.hledik@metrohm.com>
2025-07-10 22:08:51 +0200

    attempt at changing the prefix

--- exporters/css/src/content/token.ts
+++ exporters/css/src/content/token.ts
@@ -79,7 +79,6 @@ export function tokenVariableName(token: Token, tokenGroups: Array<TokenGroup>, 
     token,
     exportConfiguration.tokenNameStyle,
     exportConfiguration.tokenNameStructure !== TokenNameStructure.NameOnly ? parent : null,
-    // [exportConfiguration.globalNamePrefix, prefix, collection?.name].filter(Boolean).join('-')
-    [exportConfiguration.globalNamePrefix, collection?.name].filter(Boolean).join('-')
+    [exportConfiguration.globalNamePrefix, "example", collection?.name].filter(Boolean).join('-')
   )
 }
